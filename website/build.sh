841594994456588c2eb903fe74f4f53ca140abd4
arseniyacyna2010-ux <arseniyacyna2010@gmail.com>
2026-02-26 20:03:47 +0700

    Add files via upload

---
+++ website/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Установка зависимостей
+pip install -r requirements.txt
+
+# Сбор статических файлов
+python manage.py collectstatic --no-input
+
+# Применение миграций базы данных
+python manage.py migrate
